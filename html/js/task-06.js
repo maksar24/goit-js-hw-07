fc401f3049e6d70d30bcaba8d111ce88f9566fac
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-05-03 22:52:42 +0200

    Fix task - 06 and create a base for task - 08

--- html/js/task-06.js
+++ html/js/task-06.js
@@ -1,19 +1,24 @@
 const valueInput = document.getElementById("validation-input");
 const validLength = valueInput.dataset.length;
 
-function onToggleValid() {
-    valueInput.classList.toggle("valid");
+function validInputName(event) {
+    if (valueInput.value.length !== Number(validLength)) {
+            return onAddInvalid(event);
+    };
+    return onAddValid(event);
 };
 
-function onToggleInvalid() {
-    valueInput.classList.toggle("invalid");
+function onAddValid() {
+    valueInput.classList.add("valid");
 };
 
-function validInputName() {
-    if (valueInput.value.length === validLength) {
-        return onToggleValid;
-    }
-    return onToggleInvalid;
+function onAddInvalid() {
+    valueInput.classList.add("invalid");
+};
+
+function onRemoveClass() {
+    valueInput.classList.remove(...valueInput.classList);
 };
 
 valueInput.addEventListener("blur", validInputName);
+valueInput.addEventListener("focus", onRemoveClass);
